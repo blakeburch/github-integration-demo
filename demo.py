92892a9bfcfe10a807c725c5bf941cd514f87e91
Blake Burch <blake@shipyardapp.com>
2020-12-08 16:18:40 -0600

    Convert Env Var to Int

--- demo.py
+++ demo.py
@@ -8,8 +8,8 @@ print(f'Hello, {name}')
 pandas_version = pd.__version__
 print(f'You currently have version {pandas_version} of Pandas installed. This was pulled in from your requirements.txt file.')
 
-max_number = os.environ.get('MAX',1000)
-rows = os.environ.get('ROWS', 1000)
+max_number = int(os.environ.get('MAX',1000))
+rows = int(os.environ.get('ROWS', 1000))
 print(f'This is a randomly generated dataframe with {rows} rows and numbers between 0 and {max_number}.')
 df = pd.DataFrame(np.random.randint(0,max_number,size=(rows, 4)), columns=list('ABCD'))
 print(df)
