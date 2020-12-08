04a23498f6c2592ddd305d0ace1c5b96e0801f51
Blake Burch <blake@shipyardapp.com>
2020-12-08 16:06:23 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,5 +11,5 @@ print(f'You currently have version {pandas_version} of Pandas installed. This wa
 max_number = os.environ.get('MAX',1000)
 rows = os.environ.get('ROWS', 1000)
 print(f'This is a randomly generated dataframe with {rows} rows and numbers between 0 and {max_number}.')
-df = pd.DataFrame(np.random.randint(0,max_number,size=(max_number, 4)), columns=list('ABCD'))
+df = pd.DataFrame(np.random.randint(0,max_number,size=(rows, 4)), columns=list('ABCD'))
 print(df)
